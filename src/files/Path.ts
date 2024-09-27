7e3a41c8c25f2f2d5ef89d4adea52b9a51629374
KevinPlease <kevin_beqo@hotmail.com>
2024-09-28 01:31:25 +0200

    - File service additions, fixes.

--- src/files/Path.ts
+++ src/files/Path.ts
@@ -1,7 +1,9 @@
 import { MessengerFunction } from "../Messenger";
 
 export default {
-	
+
+	FS_SEPARATOR: process.platform === "win32" ? "\\" : "/",
+
 	rootOf: (appendix: string, say: MessengerFunction): string => {
 		return say("Path", "ask", "rootPathOf", appendix);
 	}
