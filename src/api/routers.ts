da871256cd5026c0fb7fd9f63df951e62436061a
KevinPlease <kevin_beqo@hotmail.com>
2024-10-28 21:17:46 +0100

    - Added RoleRouter.

--- src/api/routers.ts
+++ src/api/routers.ts
@@ -1,7 +1,8 @@
 import { Router } from "../network/Router";
 import AuthRouter from "./auth/AuthRouter";
+import RoleRouter from "./role/RoleRouter";
 import UserRouter from "./user/UserRouter";
 
-const ROUTERS: typeof Router[] = [ AuthRouter, UserRouter ];
+const ROUTERS: typeof Router[] = [ AuthRouter, UserRouter, RoleRouter ];
 
 export { ROUTERS };
