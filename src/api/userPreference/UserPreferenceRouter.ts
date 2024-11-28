709efdadb8d913daff56260c6ea52e2c2668e53f
KevinPlease <kevin_beqo@hotmail.com>
2024-11-28 23:50:25 +0100

    - [UserPreferences] Added the core.

---
+++ src/api/userPreference/UserPreferenceRouter.ts
@@ -0,0 +1,20 @@
+import { Router as ExpRouter } from "express";
+
+import UserPreferenceController from "./UserPreferenceController";
+import { MessengerFunction } from "../../Messenger";
+import { RawRoutesInfo } from "../../network/types/RoutesInfo";
+import { Router } from "../../network/Router";
+import ROUTES from "./routes";
+
+class UserPreferenceRouter extends Router {
+    constructor(router: ExpRouter, routerMany: ExpRouter, controller: UserPreferenceController, routes: RawRoutesInfo, msngr: MessengerFunction) {
+        super(router, routerMany, controller, routes, msngr);
+    }
+
+    static create(expRouter: ExpRouter, expRouterMany: ExpRouter, say: MessengerFunction): UserPreferenceRouter {
+        const controller = new UserPreferenceController(say);
+        return new UserPreferenceRouter(expRouter, expRouterMany, controller, ROUTES, say);
+    }
+}
+
+export default UserPreferenceRouter;
