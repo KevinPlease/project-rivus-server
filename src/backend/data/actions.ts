760f4a4bb1132c8598bc200d3d627755e6107c1d
KevinPlease <kevin_beqo@hotmail.com>
2025-01-18 00:26:27 +0100

    - Default action data fixes/unification.

--- src/backend/data/actions.ts
+++ src/backend/data/actions.ts
@@ -5,216 +5,188 @@ const getDefaultData = (domain: string) => {
 	domain = ExString.uncapitalize(domain);
 	return [
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db596"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Public Action",
-				"description": "No special privileges."
+			_id: new ObjectId("66bfacc443c23b7cb68f2961"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Public Action",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636331,
+				timeUpdated: 1723837636331,
+				creator: "21:47:16 -- undefined:Function : undefined"
 			}
 		},
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db597"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "CRUD Actions",
-				"description": "CRUD operations on System Actions."
+			_id: new ObjectId("66bfacc443c23b7cb68f2976"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Remove User",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636332,
+				timeUpdated: 1723837636332,
+				creator: "21:47:16 -- undefined:Function : undefined"
 			}
 		},
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db598"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Get Form Details",
-				"description": ""
+			_id: new ObjectId("66bfacc443c23b7cb68f2977"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Get User",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636332,
+				timeUpdated: 1723837636332,
+				creator: "21:47:16 -- undefined:Function : undefined"
 			}
 		},
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db599"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Add User",
-				"description": "Add a user in the database."
+			_id: new ObjectId("66bfacc443c23b7cb68f2978"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Get Users",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636332,
+				timeUpdated: 1723837636332,
+				creator: "21:47:16 -- undefined:Function : undefined"
 			}
 		},
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db59a"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Get User",
-				"description": ""
+			_id: new ObjectId("66bfacc443c23b7cb68f2979"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Add User",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636332,
+				timeUpdated: 1723837636332,
+				creator: "21:47:16 -- undefined:Function : undefined"
 			}
 		},
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db59b"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Get Users",
-				"description": ""
+			_id: new ObjectId("66bfacc443c23b7cb68f297a"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Add User Images",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636332,
+				timeUpdated: 1723837636332,
+				creator: "21:47:16 -- undefined:Function : undefined"
 			}
 		},
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db59c"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Edit User",
-				"description": ""
+			_id: new ObjectId("66bfacc443c23b7cb68f297b"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Edit User",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636332,
+				timeUpdated: 1723837636332,
+				creator: "21:47:16 -- undefined:Function : undefined"
 			}
 		},
 		{
-			"_id": new ObjectId("64b877be20a24bc2e25db59d"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Remove User",
-				"description": ""
+			_id: new ObjectId("66f71e4008f07f5100cc2424"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Get Order Pdf",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1723837636331,
+				timeUpdated: 1723837636331,
+				creator: ""
 			}
 		},
 		{
-			"_id": new ObjectId("64de9f0053f071842e689392"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Add User Images",
-				"description": ""
+			_id: new ObjectId("6719777686bb1afe1bb039c7"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Remove Role",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1715626111,
+				timeUpdated: 1715626111,
+				creator: ""
 			}
 		},
 		{
-			"_id": new ObjectId("64deacdbf401114920c1dc6a"),
-			"repository": `actions@${domain}_sys`,
-			"role": "action",
-			"data": {
-				"name": "Edit Own User",
-				"description": ""
+			_id: new ObjectId("6719777686bb1afe1bb039c8"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Get Role",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1715626111,
+				timeUpdated: 1715626111,
+				creator: ""
 			}
 		},
 		{
-			"_id": new ObjectId("6719777686bb1afe1bb039c7"),
-			"repository": "actions@rivus_sys",
-			"role": "action",
-			"data": {
-				"name": "Remove Role",
-				"description": ""
+			_id: new ObjectId("6719777686bb1afe1bb039c9"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Get Roles",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1715626111,
+				timeUpdated: 1715626111,
+				creator: ""
 			}
 		},
 		{
-			"_id": new ObjectId("6719777686bb1afe1bb039c8"),
-			"repository": "actions@rivus_sys",
-			"role": "action",
-			"data": {
-				"name": "Get Role",
-				"description": ""
+			_id: new ObjectId("6719777686bb1afe1bb039ca"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Add Role",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1715626111,
+				timeUpdated: 1715626111,
+				creator: ""
 			}
 		},
 		{
-			"_id": new ObjectId("6719777686bb1afe1bb039c9"),
-			"repository": "actions@rivus_sys",
-			"role": "action",
-			"data": {
-				"name": "Get Roles",
-				"description": ""
+			_id: new ObjectId("6719777686bb1afe1bb039cb"),
+			repository: `actions@${domain}_sys`,
+			role: "action",
+			data: {
+				name: "Edit Role",
+				description: ""
 			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
-			}
-		},
-		{
-			"_id": new ObjectId("6719777686bb1afe1bb039ca"),
-			"repository": "actions@rivus_sys",
-			"role": "action",
-			"data": {
-				"name": "Add Role",
-				"description": ""
-			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
-			}
-		},
-		{
-			"_id": new ObjectId("6719777686bb1afe1bb039cb"),
-			"repository": "actions@rivus_sys",
-			"role": "action",
-			"data": {
-				"name": "Edit Role",
-				"description": ""
-			},
-			"meta": {
-				"timeCreated": 1715626111,
-				"timeUpdated": 1715626111,
-				"creator": ""
+			meta: {
+				timeCreated: 1715626111,
+				timeUpdated: 1715626111,
+				creator: ""
 			}
 		}
-	]
+	];
 }
 
 export { getDefaultData };
